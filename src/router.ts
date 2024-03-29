785a9a8a2a7411d7bbab21c496cf05223f8f7683
Angel Ivanov <angel.iwanov@gmail.com>
2024-03-29 16:09:03 +0200

    handlers for update

--- src/router.ts
+++ src/router.ts
@@ -1,43 +1,32 @@
 import { Router } from 'express';
-import { body, oneOf } from 'express-validator';
+import { body } from 'express-validator';
 import { handleInputErros } from './modules/middlewares';
+import { createProduct, deleteProduct, getOneProduct, getProducts, updateProduct } from './handlers/product';
+import { createUpdate, deleteUpdate, getOneUpdate, getUpdates, updateUpdate } from './handlers/update';
 
 const router = Router();
 
 // Product
-router.get('/product', (req, res) => {
-  res.json({message: 'message'})
-})
-router.get('/product/:id', () => {})
-router.put('/product/:id', body('name').isString(), handleInputErros, (req, res) => {
-  res.status(200);
-  res.json({message: "product successfully update"});
-})
-router.post('/product', body('name').isString(), handleInputErros, (req, res) => {
-  res.status(200);
-  res.json({message: "product successfully created"});
-})
-router.delete('/product/:id', () => {})
+router.get('/product', getProducts);
+router.get('/product/:id', getOneProduct)
+router.put('/product/:id', body('name').isString(), handleInputErros, updateProduct)
+router.post('/product', body('name').isString(), handleInputErros, createProduct)
+router.delete('/product/:id', deleteProduct)
 
 // Update
-router.get('/update', () => {})
-router.get('/update/:id', () => {})
+router.get('/update', getUpdates)
+router.get('/update/:id', getOneUpdate)
 router.put('/update/:id', [
   body('title').optional(),
   body('body').optional(),
   body('status').isIn(["IN_PROGESS", "SHIPPED", "DEPRECATED"]),
   body('version').optional()], 
-  handleInputErros, (req, res) => {
-    res.status(200);
-    res.json({message: "successful update"});
-})
+  handleInputErros, updateUpdate)
 router.post('/update', [
   body('title').exists().isString(),
-  body('body').exists().isString()], handleInputErros, (req, res) => {
-  res.status(200);
-  res.json({message: "successful update"});
-})
-router.delete('/update/:id', () => {})
+  body('body').exists().isString(),
+  body('productId').exists().isString(),], handleInputErros, createUpdate)
+router.delete('/update/:id', deleteUpdate)
 
 // Update Point
 router.get('/updatepoint', () => {})
